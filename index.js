30cc6a5bae133398a1057a8d380d7cc150ebaf2e
LucasASC23 <139932248+LucasASC23@users.noreply.github.com>
2025-10-15 16:46:34 +0000

    Make fetch data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Task 1
 // Define a function called getDogArtworks that makes the API call to the request URL you defined in the worksheet (to get 3 dog artworks)
 // Make sure to console log all 3 titles (after you get the data retrieve the nested values) 
+const requestURL="https://api.artic.edu/api/v1/artworks/search?=dog&limit=31"
 
 
+async function fetchData(requestUrl){
+    const response= await fetch(requestUrl)
+    const data=await response.json()
+    let i=0
+    while(i<3){
+
+        console.log(data.data[i].title)
+        i++
+    }
+
+}
 
 // Call the function and confirm it worked correctly
-
+fetchData(requestUrl)
 
 // Task 2
 // Define another function called searchArtworks that takes in a search query and a size limit
@@ -14,6 +26,11 @@
 // Now when the function it's called it should only return artwork the specified # of artworks based on the inputed search query
 // Make sure to console log the values. 
 
+async function searchArtworks(query,size){
+    let requestUrl=
+   
+
+}
 
 
 
